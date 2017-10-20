66330ad2e74385446aac647e4d6f335fbb19877a
Maximilian Merte <maximilian.merte@rwth-aachen.de>
2017-10-20 16:06:03 +0200

    basis Coefficient

--- src/input.f90
+++ src/input.f90
@@ -1,7 +1,7 @@
 module input
 	use mathematics,	only:	dp, i_dp
 	use sysPara
-	use output,		 	only:		writeEnAndUNK 
+	use output,		 	only:		writeEnAndCK
 
 
 	implicit none
@@ -17,28 +17,28 @@ module input
 
 !public:
 	logical function filesExist()
-		logical				:: unkR, unkI, En
+		logical				:: ckR, ckI, En
 		!
-		inquire( file="./rawData/unkR.dat"			, exist=unkR	)
-		inquire( file="./rawData/unkI.dat"			, exist=unkI	)
+		inquire( file="./rawData/ckR.dat"			, exist=ckR	)
+		inquire( file="./rawData/ckI.dat"			, exist=ckI	)
 		inquire( file="./rawData/bandStruct.dat"	, exist=En		)
 		!
-		if( .not. unkR ) write(*,*)"[filesExist]: real part of unk not found"
-		if( .not. unkI ) write(*,*)"[filesExist]: imag part of unk not found"
+		if( .not. ckR ) write(*,*)"[filesExist]: real part of basCoeff not found"
+		if( .not. ckI ) write(*,*)"[filesExist]: imag part of basCoeff not found"
 		if( .not. En   ) write(*,*)"[filesExist]: energies not found"
 		!
-		filesExist = ( unkR .and. unkI )  .and. En
+		filesExist = ( ckR .and. ckI )  .and. En
 		!
 		return
 	end function
 
 
-	subroutine	readHam(unk, En)
-		complex(dp),	intent(out)		:: unk(:,:,:)
+	subroutine	readHam(ck, En)
+		complex(dp),	intent(out)		:: ck(:,:,:)
 		real(dp),		intent(out)		:: En(:,:)
 		real(dp),		allocatable		:: buffer(:,:,:), eBuff(:,:)
 		!
-		allocate(	buffer( size(unk,1), size(unk,2), size(unk,3) )		)
+		allocate(	buffer( size(ck,1), size(ck,2), size(ck,3) )		)
 		allocate(	eBuff(nG,nQ)										)
 		!
 		!
@@ -46,17 +46,17 @@ module input
 			!
 			!UNK REAL PART
 			buffer	= 0.0_dp
-			open(unit=700, file="./rawData/unkR.dat",form='unformatted',access='stream',action='read')
+			open(unit=700, file="./rawData/ckR.dat",form='unformatted',access='stream',action='read')
 				read(700) buffer
 			close(700,status='delete')
-			unk	= buffer
+			ck	= buffer
 			!
 			!UNK IMAG PART
 			buffer	= 0.0_dp
-			open(unit=710, file="./rawData/unkI.dat",form='unformatted',access='stream',action='read')
+			open(unit=710, file="./rawData/ckI.dat",form='unformatted',access='stream',action='read')
 				read(710) buffer
 			close(710,status='delete')
-			unk	= unk + i_dp * buffer
+			ck	= ck + i_dp * buffer
 			!
 			!BAND ENERGIES
 			open(unit=720, file="./rawData/bandStruct.dat",form='unformatted',access='stream',action='read')
@@ -65,10 +65,10 @@ module input
 			En(1:nBands,:)	= eBuff(1:nBands,:)
 			!
 			!
-			call writeEnAndUNK(eBuff, unk)
+			call writeEnAndCK(eBuff, ck)
 		else
 			write(*,*)	"[readHam]: could not find all necessary files"
-			unk	= dcmplx(0.0_dp)
+			ck	= dcmplx(0.0_dp)
 			En	= 0.0_dp
 		end if
 		!
