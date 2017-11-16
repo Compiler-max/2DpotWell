f00e060229c6687f5b9ed25db86a93863083e422
Maximilian Merte <maximilian.merte@rwth-aachen.de>
2017-11-16 10:11:08 +0100

    striped numerical routines

--- src/projection.f90
+++ src/projection.f90
@@ -11,7 +11,7 @@ module projection
 	implicit none	
 	
 	private
-	public ::	projectUnk, calcAmatANA		
+	public ::	calcAmatANA		
 
 
 
@@ -28,226 +28,7 @@ module projection
 
 	contains
 !public:
-
-	subroutine projectUnk(ckH, ckW, Uq)	!projectBwf(qi, bWf, loBwf, U(:,:), failCount, smin, smax)
-		!does the projection onto Loewdin-orthonormalized Bloch-like states
-		!see Marzari, Vanderbilt PRB 56, 12847 (1997) Sec.IV.G.1 for detailed description of the method 
-		complex(dp)	,	intent(in)		:: ckH(:,:,:)  ! 	ck(nG,nBands  ,	nQ)		
-		complex(dp)	,	intent(out)		:: ckW(:,:,:), Uq(:,:,:)	! Uq(nBands	,nWfs, 	nQ	)	
-		real(dp),		allocatable		:: EnP(:,:)	
-		complex(dp)	,	allocatable		:: loBwf(:,:), gnr(:,:), A(:,:), Ham(:,:), tmp(:,:)
-		
-		integer							:: qi, n, m, gi
-		!
-		allocate(	loBwf(nR,nWfs)		)
-		allocate(	gnr(nR,nWfs)		)
-		allocate( 	A(nBands,nWfs)		)
-		allocate(	Ham(nWfs,nWfs)		)
-		allocate(	tmp(nBands,nWfs))
-		allocate(	EnP(nWfs,nQ)		)
-		!
-		if( debugProj ) then
-			write(*,*)	"[projectUnk]: debug mode, will test unk normalization after debuging"
-		end if
-		!
-		!
-
-		if( doProjNUM) then
-			call genTrialOrb(gnr)
-			write(*,*)	"[projectUnk]: generated trial orbitals"
-		end if
-		!
-		!PROJECT
-		do qi = 1, nQ
-			if( doProj ) then 
-				Uq(:,:,qi)	= dcmplx(0.0_dp)
-				call calcAmat(qi,ckH(:,:,qi) ,gnr, A) 
-				call calcUmat(A, Uq(:,:,qi))
-				!ROTATE BLOCH STATES
-				do gi = 1, nGq(qi)
-					ckW(gi,:,qi)	= matmul( ckH(gi,:,qi), Uq(:,:,qi))
-				end do
-				!ckW(:,:,qi)	= matmul( ckH(:,:,qi) , Uq(:,:,qi)	 ) !			
-				!do n = 1, nWfs
-				!	do m = 1, nWfs
-				!		do gi = 1, nGq(qi)
-				!			ckW(gi,n,qi)	= ckW(gi,n,qi) + ckH(gi,m,qi) * Uq(m,n,qi)
-				!		end do
-				!	end do
-				!end do
-			else
-				write(*,*)	"[projectUNK]: projection disabled, U= Identity"
-				
-				Uq(:,:,qi) = dcmplx(0.0_dp)
-				do n = 1, size(Uq,1)
-					do m = 1, size(Uq,2)
-						if(n==m) then
-							Uq(n,m,qi)	= dcmplx(1.0_dp)
-						end if
-					end do
-				end do
-				ckW(:,:,qi) = ckH(:,:,qi)
-			end if
-			!
-			
-
-			if( debugProj  ) then
-				!if( .not. isUnit(ckW(:,:,qi))	)	write(*,*) "[projectUNK]: ckW not a unitary matrix at qi=",qi
-				if( .not. isUnit(Uq(:,:,qi)) 	) 	write(*,*) "[projectUNK]: Uq not a unitary matrix at qi=",qi
-			end if
-
-		end do
-		!
-
-		write(*,*)	"[projectUnk]: done with projections at each k point"	
-		!
-		return
-	end subroutine
-		
-
-
-
-	subroutine addThopp(qi, U,En, tHopp)
-		!	<0|H|R> = (1/sqrt(N)) \sum_q Vq^dagger Eq Vq
-		!	where Eq is diagonal matrix (energies in Hamiltonian gauge)
-		integer,		intent(in)		:: qi
-		complex(dp),	intent(in)		:: U(:,:)	!	U(nBands,nWfs)
-		real(dp),		intent(in)		:: En(:,:)	!	En(nBands,nQ)
-		complex(dp),	intent(inout)	:: tHopp(:,:,:)	!tHopp(nWfs, nWfs nSC)
-		complex(dp)						:: phase
-		complex(dp),	allocatable		:: tmp(:,:),EV(:,:), Udag(:,:)
-		integer							:: R, m, n
-		!
-		allocate(	tmp(nBands,nWfs)	)
-		allocate(	EV(nWfs,nWfs) 		)
-		allocate(	Udag(nWfs,nBands)	)
-		!		
-		!
-		do R = 1, nSC
-			!tmp = Eq Vq
-			do m = 1, nWfs
-				do n = 1, nBands
-					tmp(n,m)	= dcmplx(En(n,qi)) * U(n,m)
-				end do 
-			end do
-			! EU = Vq^dagger tmp
-			Udag	= dconjg( transpose(U)	)
-			EV		= matmul(Udag, tmp)
-			!ADD results
-			phase			= myExp( - dot_product( qpts(:,qi), Rcell(:,R) ) 		)
-			tHopp(:,:,R)	= tHopp(:,:,R) + phase * EV(:,:) /  dcmplx(real(nSC,dp))
-		end do
-		!
-		!tHopp	= tHopp    
-		!
-		return
-	end subroutine
-
-
-	
-
-
-
-
-!privat:
-	subroutine genTrialOrb(gnr)
-		complex(dp),	intent(out)	:: gnr(:,:)       !gnr( nR, nWfs)	
-		integer						:: n, ri, gammaP
-		!
-		if(nWfs > nBands) then
-			write(*,*)"[genTrialOrb]: warning, nWfs larger then nBands! No propper subspace..."
-		end if
-		!
-		gammaP	= getGammaPoint()
-		gnr 	= dcmplx(0.0_dp)
-		!
-		!SINGLE ATOM
-		if( nAt == 1 ) then
-			write(*,*)	"[genTrialOrb]: assuming a one atom per unit cell" 
-			do n = 1, nWfs
-				do ri = 1, nR
-					if( 0.0_dp < rpts(1,ri) .and.  rpts(1,ri) < aX .and. 0.0_dp < rpts(2,ri) .and. rpts(2,ri) < aY) then
-						if( 	insideAt(1, rpts(:,ri)))	then
-							gnr(ri,n)	= potWell(1,n,ri)
-						end if
-					end if
-				end do
-			end do
-		!TWO ATOMS
-		else if( nAt == 2) then
-			write(*,*)	"[genTrialOrb]: assuming a two atoms per unit cell" 
-			do n = 1, nWfs-1, 2
-				do ri = 1, nR
-					if( 0.0_dp < rpts(1,ri) .and.  rpts(1,ri) < aX .and. 0.0_dp < rpts(2,ri) .and. rpts(2,ri) < aY) then
-						if( 	insideAt(1, rpts(:,ri)))	then
-							gnr(ri,n)	= potWell(1,n,ri)
-						else if( insideAt(2, rpts(:,ri)) ) then
-							gnr(ri,n+1) = potWell(2,n,ri)
-						end if
-					end if
-				end do
-			end do
-		!DEFAULT		
-		else
-			write(*,*)	"[genTrialOrb]: to many atoms per unit cell for trial orbitals" 
-		end if
-		!
-		return
-	end subroutine
-
-
-
-	complex(dp) function potWell(at, n, ri)
-		integer, 	intent(in)		:: at, n, ri
-		real(dp)					:: x,y, Lx, Ly, xc, yc, kx, ky, A
-		!
-		x	= rpts(1,ri)
-		y	= rpts(2,ri)
-		Lx 	= 2.0_dp * atR(1,at)
-		Ly 	= 2.0_dp * atR(2,at)
-		xc	= atPos(1,at)
-		yc	= atPos(2,at)
-		kx	= PI_dp/ Lx
-		ky	= PI_dp/ Ly
-		A	= dsqrt(4.0_dp / (Lx * Ly)	)
-		!
-		if( n==1 ) then
-			potWell = A * dsin( kx*(x -xc + 0.5_dp * Lx) ) * dsin( ky*(y -yc + 0.5_dp * Ly) )
-		else if( n==2 ) then
-			potWell = A * dcos( kx*(x -xc + 0.5_dp * Lx) ) * dsin( ky*(y -yc + 0.5_dp * Ly) )
-		else if( n==3 ) then
-			potWell = A * dsin( kx*(x -xc + 0.5_dp * Lx) ) * dcos( ky*(y -yc + 0.5_dp * Ly) )
-		end if 
-
-
-		return
-	end function
-
-
-
-
-
-
-
-	subroutine calcAmat(qi,ckH ,gnr, A)
-		integer,		intent(in)	:: qi
-		complex(dp),	intent(in)	:: ckH(:,:), gnr(:,:) 
-		complex(dp),	intent(out)	:: A(:,:) !A(nBands,nWfs)
-		!calculates the inner product matrix of bwfs and trial orbital gnr
-		if( doProjNUM ) then
-			if( qi==1 )		write(*,*)"[calcAmat]: numeric calculation of projection matrix A"
-			call calcAmatNUM(qi, ckH, gnr, A)
-		else
-			if( qi == 1)	write(*,*)"[calcAmat]: analytic calculation of projection matrix A"
-			call calcAmatANA(qi, ckH, A)
-		end if
-		!
-		!
-		return
-	end subroutine
-
-
-	subroutine calcAmatANA(qi,ckH, A)
+		subroutine calcAmatANA(qi,ckH, A)
 		!analytic projection with hard coded integrals
 		!	projection onto sin**2, sin cos, cos sin
 		integer,		intent(in)	:: qi
@@ -310,6 +91,8 @@ module projection
 	end subroutine
 
 
+
+!privat:
 	complex(dp) function g1Int(qi,m, at,ckH)
 		integer,		intent(in)	:: qi, m, at
 		complex(dp),	intent(in)	:: ckH(:,:)
@@ -508,118 +291,332 @@ module projection
 
 
 
-
-
-	subroutine calcAmatNUM(qi, ckH, gnr, A)
-		!numerical caluclation allows for quick change of trial orbitals 
-		integer,		intent(in)	:: qi
-		complex(dp),	intent(in)	:: ckH(:,:), gnr(:,:) 
-		complex(dp),	intent(out)	:: A(:,:) !A(nBands,nWfs)
-		complex(dp),	allocatable	:: psi(:), basVec(:)
-		integer						:: m,n, xi, gMax
-		!
-		allocate(	basVec(nG)	)
-		allocate(	psi(nBands)	)
-		!
-		gMax = nGq(qi)
-		!
-		!INTEGRATION OVER REAL SPACE
-		A = dcmplx(0.0_dp)
-		do xi = 1, nR
-			!GET BASIS VECTOR
-			call calcBasis(qi,xi, basVec)
-			psi(:)	= matmul( basVec(1:gMax), ckH(1:gMax,1:nBands) ) / dsqrt(vol)
-			psi(:)	= dconjg(psi)
-			!CALC OVERLAP AT CURRENT GRID POINT
-			do n = 1, nWfs
-				do m = 1 , nBands 
-					A(m,n)	= A(m,n) + psi(m) * gnr(xi,n)
-				end do 
-			end do
-		end do
-		!NORMALIZE REAL SPACE INTEGRATION
-		A	= A / real(nR,dp)
-		!
-		!
-		return
-	end subroutine
-
-
-
-
-
-
-
-
-
-	subroutine calcUmat(A, U)
-		!U = A S^-0.5 = Z I V
-		!	where A= Z d V (singular value decomposition) 
-		complex(dp),	intent(in)		:: A(:,:)	!A(nBands,nWfs)
-		complex(dp),	intent(out)		:: U(:,:)	!U(nBands,nWfs)
-		complex(dp),	allocatable		:: Z(:,:), I(:,:), V(:,:), TMP(:,:)
-		real(dp),		allocatable		:: d(:)
-		integer							:: m, n, k, lda, ldb, ldc
-		complex(dp)						:: alpha, beta
-		character*1						:: transa, transb
-		!
-		allocate(	Z(	nBands, 	nBands	)	)
-		allocate(	I(	nBands,		nWfs	)	)
-		allocate(	V(	nWfs,		nWfs	)	)
-		allocate(	TMP(nBands,		nWfs	)	)
-		allocate(	d(	nWfs				)	)
-		!
-		!SET UP IDENTITY MATRIX
-		I	= dcmplx(0.0_dp)
-		do m = 1, nBands
-			do n = 1, nWfs
-				if( n == m) then
-					I(m,n)	= dcmplx(1.0_dp)
-				end if
-			end do
-		end do
-		!
-		!CALC SVD OF A
-		call mySVD(A,Z,d,V)
-		!
-		!CALC U
-		tmp=matmul(I,V)
-		U=matmul(Z,tmp)
-
-		!calc U with mkl
-		!transa	= 'n'
-		!transb	= 'n'
-		!alpha	= dcmplx(1.0_dp)
-		!beta	= dcmplx(0.0_dp)
-		!m		= size(I,1)
-		!n		= size(V,2)
-		!k		= size(I,2)
-		!lda		= m
-		!ldb		= k
-		!ldc		= m
-		!call zgemm(transa, transb, m, n, k, alpha, I, lda, V, ldb, beta, TMP, ldc)
-		!
 		
-		!transa	= 'n'
-		!transb	= 'n'
-		!alpha	= dcmplx(1.0_dp)
-		!beta	= dcmplx(0.0_dp)
-		!m		= size(Z,1)
-		!n		= size(TMP,2)
-		!k		= size(Z,2)
-		!lda		= m
-		!ldb		= k
-		!ldc		= m
-		!call zgemm(transa, transb, m, n, k, alpha, Z, lda, TMP, ldb, beta, U, ldc)
-		!
-		!
-		return
-	end subroutine
+
+
+
+
+!DEPRECATED (NUMERICAL ON REAL SPACE GRID)
+	!subroutine addThopp(qi, U,En, tHopp)
+	!	!	<0|H|R> = (1/sqrt(N)) \sum_q Vq^dagger Eq Vq
+	!	!	where Eq is diagonal matrix (energies in Hamiltonian gauge)
+	!	integer,		intent(in)		:: qi
+	!	complex(dp),	intent(in)		:: U(:,:)	!	U(nBands,nWfs)
+	!	real(dp),		intent(in)		:: En(:,:)	!	En(nBands,nQ)
+	!	complex(dp),	intent(inout)	:: tHopp(:,:,:)	!tHopp(nWfs, nWfs nSC)
+	!	complex(dp)						:: phase
+	!	complex(dp),	allocatable		:: tmp(:,:),EV(:,:), Udag(:,:)
+	!	integer							:: R, m, n
+	!	!
+	!	allocate(	tmp(nBands,nWfs)	)
+	!	allocate(	EV(nWfs,nWfs) 		)
+	!	allocate(	Udag(nWfs,nBands)	)
+	!	!		
+	!	!
+	!	do R = 1, nSC
+	!		!tmp = Eq Vq
+	!		do m = 1, nWfs
+	!			do n = 1, nBands
+	!				tmp(n,m)	= dcmplx(En(n,qi)) * U(n,m)
+	!			end do 
+	!		end do
+	!		! EU = Vq^dagger tmp
+	!		Udag	= dconjg( transpose(U)	)
+	!		EV		= matmul(Udag, tmp)
+	!		!ADD results
+	!		phase			= myExp( - dot_product( qpts(:,qi), Rcell(:,R) ) 		)
+	!		tHopp(:,:,R)	= tHopp(:,:,R) + phase * EV(:,:) /  dcmplx(real(nSC,dp))
+	!	end do
+	!	!
+	!	!tHopp	= tHopp    
+	!	!
+	!	return
+	!end subroutine
+
+
+	!subroutine genTrialOrb(gnr)
+	!	complex(dp),	intent(out)	:: gnr(:,:)       !gnr( nR, nWfs)	
+	!	integer						:: n, ri, gammaP
+	!	!
+	!	if(nWfs > nBands) then
+	!		write(*,*)"[genTrialOrb]: warning, nWfs larger then nBands! No propper subspace..."
+	!	end if
+	!	!
+	!	gammaP	= getGammaPoint()
+	!	gnr 	= dcmplx(0.0_dp)
+	!	!
+	!	!SINGLE ATOM
+	!	if( nAt == 1 ) then
+	!		write(*,*)	"[genTrialOrb]: assuming a one atom per unit cell" 
+	!		do n = 1, nWfs
+	!			do ri = 1, nR
+	!				if( 0.0_dp < rpts(1,ri) .and.  rpts(1,ri) < aX .and. 0.0_dp < rpts(2,ri) .and. rpts(2,ri) < aY) then
+	!					if( 	insideAt(1, rpts(:,ri)))	then
+	!						gnr(ri,n)	= potWell(1,n,ri)
+	!					end if
+	!				end if
+	!			end do
+	!		end do
+	!	!TWO ATOMS
+	!	else if( nAt == 2) then
+	!		write(*,*)	"[genTrialOrb]: assuming a two atoms per unit cell" 
+	!		do n = 1, nWfs-1, 2
+	!			do ri = 1, nR
+	!				if( 0.0_dp < rpts(1,ri) .and.  rpts(1,ri) < aX .and. 0.0_dp < rpts(2,ri) .and. rpts(2,ri) < aY) then
+	!					if( 	insideAt(1, rpts(:,ri)))	then
+	!						gnr(ri,n)	= potWell(1,n,ri)
+	!					else if( insideAt(2, rpts(:,ri)) ) then
+	!						gnr(ri,n+1) = potWell(2,n,ri)
+	!					end if
+	!				end if
+	!			end do
+	!		end do
+	!	!DEFAULT		
+	!	else
+	!		write(*,*)	"[genTrialOrb]: to many atoms per unit cell for trial orbitals" 
+	!	end if
+	!	!
+	!	return
+	!end subroutine
+
+
+
+	!complex(dp) function potWell(at, n, ri)
+	!	integer, 	intent(in)		:: at, n, ri
+	!	real(dp)					:: x,y, Lx, Ly, xc, yc, kx, ky, A
+	!	!
+	!	x	= rpts(1,ri)
+	!	y	= rpts(2,ri)
+	!	Lx 	= 2.0_dp * atR(1,at)
+	!	Ly 	= 2.0_dp * atR(2,at)
+	!	xc	= atPos(1,at)
+	!	yc	= atPos(2,at)
+	!	kx	= PI_dp/ Lx
+	!	ky	= PI_dp/ Ly
+	!	A	= dsqrt(4.0_dp / (Lx * Ly)	)
+	!	!
+	!	if( n==1 ) then
+	!		potWell = A * dsin( kx*(x -xc + 0.5_dp * Lx) ) * dsin( ky*(y -yc + 0.5_dp * Ly) )
+	!	else if( n==2 ) then
+	!		potWell = A * dcos( kx*(x -xc + 0.5_dp * Lx) ) * dsin( ky*(y -yc + 0.5_dp * Ly) )
+	!	else if( n==3 ) then
+	!		potWell = A * dsin( kx*(x -xc + 0.5_dp * Lx) ) * dcos( ky*(y -yc + 0.5_dp * Ly) )
+	!	end if 
+	!	!
+	!	!
+	!	return
+	!end function
 
 
 
 
 
+
+
+	!subroutine calcAmat(qi,ckH ,gnr, A)
+	!	integer,		intent(in)	:: qi
+	!	complex(dp),	intent(in)	:: ckH(:,:), gnr(:,:) 
+	!	complex(dp),	intent(out)	:: A(:,:) !A(nBands,nWfs)
+	!	!calculates the inner product matrix of bwfs and trial orbital gnr
+	!	if( doProjNUM ) then
+	!		if( qi==1 )		write(*,*)"[calcAmat]: numeric calculation of projection matrix A"
+	!		call calcAmatNUM(qi, ckH, gnr, A)
+	!	else
+	!		if( qi == 1)	write(*,*)"[calcAmat]: analytic calculation of projection matrix A"
+	!		call calcAmatANA(qi, ckH, A)
+	!	end if
+	!	!
+	!	!
+	!	return
+	!end subroutine
+
+
+
+
+
+
+	!subroutine calcAmatNUM(qi, ckH, gnr, A)
+	!	!numerical caluclation allows for quick change of trial orbitals 
+	!	integer,		intent(in)	:: qi
+	!	complex(dp),	intent(in)	:: ckH(:,:), gnr(:,:) 
+	!	complex(dp),	intent(out)	:: A(:,:) !A(nBands,nWfs)
+	!	complex(dp),	allocatable	:: psi(:), basVec(:)
+	!	integer						:: m,n, xi, gMax
+	!	!
+	!	allocate(	basVec(nG)	)
+	!	allocate(	psi(nBands)	)
+	!	!
+	!	gMax = nGq(qi)
+	!	!
+	!	!INTEGRATION OVER REAL SPACE
+	!	A = dcmplx(0.0_dp)
+	!	do xi = 1, nR
+	!		!GET BASIS VECTOR
+	!		call calcBasis(qi,xi, basVec)
+	!		psi(:)	= matmul( basVec(1:gMax), ckH(1:gMax,1:nBands) ) / dsqrt(vol)
+	!		psi(:)	= dconjg(psi)
+	!		!CALC OVERLAP AT CURRENT GRID POINT
+	!		do n = 1, nWfs
+	!			do m = 1 , nBands 
+	!				A(m,n)	= A(m,n) + psi(m) * gnr(xi,n)
+	!			end do 
+	!		end do
+	!	end do
+	!	!NORMALIZE REAL SPACE INTEGRATION
+	!	A	= A / real(nR,dp)
+	!	!
+	!	!
+	!	return
+	!end subroutine
+
+
+
+
+
+
+
+
+
+	!subroutine calcUmat(A, U)
+	!	!U = A S^-0.5 = Z I V
+	!	!	where A= Z d V (singular value decomposition) 
+	!	complex(dp),	intent(in)		:: A(:,:)	!A(nBands,nWfs)
+	!	complex(dp),	intent(out)		:: U(:,:)	!U(nBands,nWfs)
+	!	complex(dp),	allocatable		:: Z(:,:), I(:,:), V(:,:), TMP(:,:)
+	!	real(dp),		allocatable		:: d(:)
+	!	integer							:: m, n, k, lda, ldb, ldc
+	!	complex(dp)						:: alpha, beta
+	!	character*1						:: transa, transb
+	!	!
+	!	allocate(	Z(	nBands, 	nBands	)	)
+	!	allocate(	I(	nBands,		nWfs	)	)
+	!	allocate(	V(	nWfs,		nWfs	)	)
+	!	allocate(	TMP(nBands,		nWfs	)	)
+	!	allocate(	d(	nWfs				)	)
+	!	!
+	!	!SET UP IDENTITY MATRIX
+	!	I	= dcmplx(0.0_dp)
+	!	do m = 1, nBands
+	!		do n = 1, nWfs
+	!			if( n == m) then
+	!				I(m,n)	= dcmplx(1.0_dp)
+	!			end if
+	!		end do
+	!	end do
+	!	!
+	!	!CALC SVD OF A
+	!	call mySVD(A,Z,d,V)
+	!	!
+	!	!CALC U
+	!	tmp=matmul(I,V)
+	!	U=matmul(Z,tmp)
+!
+!	!	!calc U with mkl
+!	!	!transa	= 'n'
+!	!	!transb	= 'n'
+!	!	!alpha	= dcmplx(1.0_dp)
+!	!	!beta	= dcmplx(0.0_dp)
+!	!	!m		= size(I,1)
+!	!	!n		= size(V,2)
+!	!	!k		= size(I,2)
+!	!	!lda		= m
+!	!	!ldb		= k
+!	!	!ldc		= m
+!	!	!call zgemm(transa, transb, m, n, k, alpha, I, lda, V, ldb, beta, TMP, ldc)
+!	!	!
+!	!	
+!	!	!transa	= 'n'
+!	!	!transb	= 'n'
+!	!	!alpha	= dcmplx(1.0_dp)
+!	!	!beta	= dcmplx(0.0_dp)
+!	!	!m		= size(Z,1)
+!	!	!n		= size(TMP,2)
+!	!	!k		= size(Z,2)
+!	!	!lda		= m
+!	!	!ldb		= k
+!	!	!ldc		= m
+!	!	!call zgemm(transa, transb, m, n, k, alpha, Z, lda, TMP, ldb, beta, U, ldc)
+!	!	!
+!	!	!
+!	!	return
+	!end subroutine
+
+
+
+
+!	subroutine projectUnk(ckH, ckW, Uq)	!projectBwf(qi, bWf, loBwf, U(:,:), failCount, smin, smax)
+!		!does the projection onto Loewdin-orthonormalized Bloch-like states
+!		!see Marzari, Vanderbilt PRB 56, 12847 (1997) Sec.IV.G.1 for detailed description of the method
+!		!	deprecated 
+!		complex(dp)	,	intent(in)		:: ckH(:,:,:)  ! 	ck(nG,nBands  ,	nQ)		
+!		complex(dp)	,	intent(out)		:: ckW(:,:,:), Uq(:,:,:)	! Uq(nBands	,nWfs, 	nQ	)	
+!		real(dp),		allocatable		:: EnP(:,:)	
+!		complex(dp)	,	allocatable		:: loBwf(:,:), gnr(:,:), A(:,:), Ham(:,:), tmp(:,:)
+!		logical							:: doProj, doProjNUM
+!		integer							:: qi, n, m, gi
+!		!
+!		allocate(	loBwf(nR,nWfs)		)
+!		allocate(	gnr(nR,nWfs)		)
+!		allocate( 	A(nBands,nWfs)		)
+!		allocate(	Ham(nWfs,nWfs)		)
+!		allocate(	tmp(nBands,nWfs))
+!		allocate(	EnP(nWfs,nQ)		)
+!		!
+!		if( debugProj ) then
+!			write(*,*)	"[projectUnk]: debug mode, will test unk normalization after debuging"
+!		end if
+!		!
+!		!
+!		doProj 		= .true.
+!		doProjNUM	= .false.
+!		if( doProjNUM) then
+!			call genTrialOrb(gnr)
+!			write(*,*)	"[projectUnk]: generated trial orbitals"
+!		end if
+!		!
+!		!PROJECT
+!		do qi = 1, nQ
+!			if( doProj ) then 
+!				Uq(:,:,qi)	= dcmplx(0.0_dp)
+!				call calcAmat(qi,ckH(:,:,qi) ,gnr, A) 
+!				call calcUmat(A, Uq(:,:,qi))
+!				!ROTATE BLOCH STATES
+!				do gi = 1, nGq(qi)
+!					ckW(gi,:,qi)	= matmul( ckH(gi,:,qi), Uq(:,:,qi))
+!				end do
+!				!ckW(:,:,qi)	= matmul( ckH(:,:,qi) , Uq(:,:,qi)	 ) !			
+!				!do n = 1, nWfs
+!				!	do m = 1, nWfs
+!				!		do gi = 1, nGq(qi)
+!				!			ckW(gi,n,qi)	= ckW(gi,n,qi) + ckH(gi,m,qi) * Uq(m,n,qi)
+!				!		end do
+!				!	end do
+!				!end do
+!			else
+!				write(*,*)	"[projectUNK]: projection disabled, U= Identity"
+!				
+!				Uq(:,:,qi) = dcmplx(0.0_dp)
+!				do n = 1, size(Uq,1)
+!					do m = 1, size(Uq,2)
+!						if(n==m) then
+!							Uq(n,m,qi)	= dcmplx(1.0_dp)
+!						end if
+!					end do
+!				end do
+!				ckW(:,:,qi) = ckH(:,:,qi)
+!			end if
+!			!
+!			if( debugProj  ) then
+!				!if( .not. isUnit(ckW(:,:,qi))	)	write(*,*) "[projectUNK]: ckW not a unitary matrix at qi=",qi
+!				if( .not. isUnit(Uq(:,:,qi)) 	) 	write(*,*) "[projectUNK]: Uq not a unitary matrix at qi=",qi
+!			end if
+!
+!		end do
+!		!
+!		write(*,*)	"[projectUnk]: done with projections at each k point"	
+!		!
+!		return
+!	end subroutine
 
 
 
