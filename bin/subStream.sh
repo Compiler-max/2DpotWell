08f7741d58fa3e911fcc404fe5c8a6d59247c44f
Maximilian Merte <maximilian.merte@rwth-aachen.de>
2018-02-02 10:24:07 +0100

    quick

--- bin/subStream.sh
+++ bin/subStream.sh
@@ -26,58 +26,80 @@ function prepareInput {
 	#create working directory
 	mkdir -p rawData
 	rm -r -f rawData/*
+
+	mkdir -p oldInput
+	rm -r -f oldInput/*
 	#i1
 	sed -i "/doSolveHam/c\    doSolveHam  = t $infoString" ./input.txt
+	sed -i "/doPrepW90/c\    doPrepW90 = f $infoString" ./input.txt
 	sed -i "/doPw90/c\    doPw90      = f $infoString" ./input.txt
 	sed -i "/doBerry/c\    doBerry     = f $infoString" ./input.txt
 	sed -i "/useRot/c\    useRot     = f $infoString" ./input.txt
 	#i2
 	cp input.txt input2.txt
 	sed -i "/doSolveHam/c\    doSolveHam  = f $infoString" ./input2.txt
-	sed -i "/doPw90/c\    doPw90      = t $infoString" ./input2.txt
-	sed -i "/doBerry/c\    doBerry     = t $infoString" ./input2.txt
+	sed -i "/doPrepW90/c\     doPrepW90 = t $infoString"	./input2.txt
+	sed -i "/doPw90/c\    doPw90      = f $infoString" ./input2.txt
+	sed -i "/doBerry/c\    doBerry     = f $infoString" ./input2.txt
 	#i3
 	cp input2.txt input3.txt
-	sed -i "/useRot/c\    useRot      = t $infoString" ./input3.txt
+	sed -i "/doPrepW90/c\     doPrepW90 = f $infoString"	./input3.txt
+	sed -i "/doBerry/c\    doBerry     = t $infoString" ./input3.txt
+	
+	cp input3.txt input4.txt
+	sed -i "/useRot/c\    useRot      = t $infoString" ./input4.txt
 
 }
 
 function runCalc {
 	#calls the exectutables
 	#echo 'calculations disabled'
+	
+
 	#electronic structure
-	./main.exe > outABin.txt
+	mpirun -n 4 ./main.exe > outABin.txt
 	wait
 	echo '['$(date +"%T")']: finished electronic structure'
 	
-	#wannier90
+	#prepW90
+	mv input.txt oldInput/inputABinit.txt
+	mv input2.txt input.txt
+	wait
+	./main.exe > out.txt
+
+	#run wannier90
+	cd w90files
 	$wannDIR/wannier90.x wf1
 	wait
 	$wannDIR/postw90.x wf1
 	wait
+	cd ..
 	echo '['$(date +"%T")']: finished wannier90 calculations'
 	
-	#post-w90
-	mv input.txt inputABinit.txt
-	mv input2.txt input.txt
-	wait
-	./main.exe > out.txt
-	wait
-	mv input.txt inputNoROT.txt
+	
+	#Berry (no rotation = random gauge)
+	mv input.txt oldInput/inputW90prep.txt
 	mv input3.txt input.txt
-	mv polOutput.txt polOutputNoROT.txt
-	mkdir rawDataNoROT
-	cp ./rawData/* rawDataNoROT
+	wait
+	./main.exe > outNoROT.txt
+	wait
+	echo '['$(date +"%T")']: finished berry random gauge'	
+
+	#Berry2 (rotation = wannier gauge)
+	mv input.txt oldInput/inputNoROT.txt
+	mv input4.txt input.txt
+	mv output/polOutput.txt output/polOutputNoROT.txt
 	wait
 	./main.exe > outROT.txt
 	wait
-	mkdir rawDataROT
-	cp ./rawData/* ./rawDataROT
-	mv input.txt inputROT.txt
-	mv polOutput.txt polOutputROT.txt
+	echo '['$(date +"%T")']: finished berry wannier gauge'	
+	
+	mv input.txt oldInput/inputROT.txt
+	mv output/polOutput.txt output/polOutputROT.txt
+	mv out*.txt output
 	wait
-	rm -r rawData rawDataROT rawDataNoROT veloBerry.txt
-	echo '['$(date +"%T")']: finished post wannier90 run'	
+	rm -r rawData 
+	echo '['$(date +"%T")']: all done'	
 }
 
 
