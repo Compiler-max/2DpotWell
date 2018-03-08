532792d4bcc771e7e154ba1af17edb8cf4ae22d2
Maximilian Merte <maximilian.merte@rwth-aachen.de>
2018-03-08 09:54:04 +0100

    make sure correct hamiltionian is used

--- scripts/probeB.sh
+++ scripts/probeB.sh
@@ -24,19 +24,22 @@ nSCx=$((nQx+1))
 nKx=$((nQx+1))
 
 
-#set numerics (for i1 & i2)
+#set numerics
 infoString='# changed by probeB.sh'
-sed -i  "/Gcut/c\   Gcut        = $gCut $infoString"	 		./input.txt
-sed -i  "/nSolve/c\   nSolve      = $nSolve $infoString"	./input.txt
-sed -i 	"/nQx/c\   nQx         = $nQx $infoString" 			./input.txt
-sed -i 	"/nQy/c\   nQy         = $nQx $infoString" 			./input.txt
-sed -i 	"/nSCx/c\   nSCx        = $nSCx $infoString" 		./input.txt
-sed -i 	"/nSCy/c\   nSCy        = $nSCx $infoString" 		./input.txt
-sed -i 	"/nKx/c\   nKx         = $nKx $infoString" 			./input.txt
-sed -i 	"/nKy/c\   nKy         = $nKx $infoString" 			./input.txt
-sed -i 	"/shells/c\   shells       = $nShell $infoString" 	./input.txt
+sed -i  "/Gcut/c\   Gcut        = $gCut 	$infoString"	 	./input.txt
+sed -i  "/nSolve/c\   nSolve      = $nSolve 	$infoString"	./input.txt
+sed -i 	"/nQx/c\   nQx         = $nQx 	$infoString" 			./input.txt
+sed -i 	"/nQy/c\   nQy         = $nQx 	$infoString" 			./input.txt
+sed -i 	"/nSCx/c\   nSCx        = $nSCx 	$infoString" 		./input.txt
+sed -i 	"/nSCy/c\   nSCy        = $nSCx 	$infoString" 		./input.txt
+sed -i 	"/nKx/c\   nKx         = $nKx 	$infoString" 			./input.txt
+sed -i 	"/nKy/c\   nKy         = $nKx 	$infoString" 			./input.txt
+sed -i 	"/shells/c\   shells       = $nShell 	$infoString" 	./input.txt
 
-
+#set Hamiltionian
+sed -i 	"/doVdesc/c\	doVdesc		= f 	$infoString"		./input.txt
+sed -i 	"/doZeeman/c\	doZeeman	= t 	$infoString"		./input.txt
+sed -i 	"/doMagHam/c\	doMagHam	= f 	$infoString"		./input.txt
 
 
 #folder to save data
@@ -54,47 +57,47 @@ printf "*\n*\n"
 
 #body
 for bfield in ${Bz[*]}; do
-		#
-		echo '['$(date +"%T")']: start Bz='$bfield'T; V1='$Vpot'; V2='$v2'; nShell='$nShell
-		#	
-		#creates directories
-		dir=$label'/b'$bfield
-		if [[ ! -e $dir ]]; then
-   		 	mkdir $dir
-   		 	echo '['$(date +"%T")']: created dir '$dir
-		else
-    		rm -r -f $dir/*
-    		echo '['$(date +"%T")']: '$dir' was wiped'
-		fi
-		#
-		#cpy relevant files to subdirectory
-		cp main.exe $dir
-		cp input.txt $dir
-		cp subStream.sh $dir
-		wait
-		#
-		#prepare input file
-		cd $dir
-		sed -i "/B0/c\   B0		= $bfield $infoString"	./input.txt	
-		wait
-		#	
-		#execute calculation
-		./subStream.sh $nProcs
-		wait
-		echo '['$(date +"%T")']: finished subStream'
-		#
-		rm ./main.exe
-		rm ./subStream.sh
-		rm -r w90files
-		rm -r output
-		rm -r oldInput
-		rm -r rawData
+	#
+	echo '['$(date +"%T")']: start Bz='$bfield'T; V1='$Vpot'; V2='$v2'; nShell='$nShell
+	#	
+	#creates directories
+	dir=$label'/b'$bfield
+	if [[ ! -e $dir ]]; then
+   	 	mkdir $dir
+   	 	echo '['$(date +"%T")']: created dir '$dir
+	else
+    	rm -r -f $dir/*
+    	echo '['$(date +"%T")']: '$dir' was wiped'
+	fi
+	#
+	#cpy relevant files to subdirectory
+	cp main.exe $dir
+	cp input.txt $dir
+	cp subStream.sh $dir
+	wait
+	#
+	#set magnetic field amplitude
+	cd $dir
+	sed -i "/B0/c\   B0		= $bfield $infoString"	./input.txt	
+	wait
+	#	
+	#execute calculation
+	./subStream.sh $nProcs
+	wait
+	echo '['$(date +"%T")']: finished subStream'
+	#
+	rm ./main.exe
+	rm ./subStream.sh
+	rm -r w90files
+	rm -r output
+	rm -r oldInput
+	rm -r rawData
 
 
-		#jump back to root
-		cd $root
-		echo '['$(date +"%T")']: done current calc'
-		printf '*\n*\n'
+	#jump back to root
+	cd $root
+	echo '['$(date +"%T")']: done current calc'
+	printf '*\n*\n'
 done
 
 echo '['$(date +"%T")']: all done, by'
