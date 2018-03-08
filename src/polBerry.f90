047dd6d08fcbd4743e53495ba9a0eb94051c1c1e
Maximilian Merte <maximilian.merte@rwth-aachen.de>
2018-03-08 09:46:52 +0100

    remove old scripts

--- src/polBerry.f90
+++ src/polBerry.f90
@@ -178,6 +178,7 @@ module pol_Berry
 	
 		call writeConnTxt( A_conn )
 		!call writeVeloHtxt( veloQ ) !huge out file !!!!
+
 		write(*,*)	"[berryMethod]: wrote k-space info files (connection & velocities)"		
 		!		
 		!	
