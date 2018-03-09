dd9b7787f8329d433edcf93cae76c05c312aba1f
Maximilian Merte <maximilian.merte@rwth-aachen.de>
2018-03-09 13:47:44 +0100

    scripts

--- scripts/bandMovie.py
+++ scripts/bandMovie.py
@@ -2,11 +2,13 @@ import os
 import os.path
 import matplotlib.pyplot as plt
 from myBandPlot import plotBands
+from niuPlot import plotNiuColor
 
 #define energy window to be plotted
 minEn = -50.0
 maxEn = +50.0
 
+nWfs 	= 6
 
 for dirpath, dirnames, filenames in os.walk("."):
 	print('search directory:',dirpath)
@@ -14,6 +16,9 @@ for dirpath, dirnames, filenames in os.walk("."):
 	if dirpath is not ".":
 		ax = plotBands(dirpath,dirpath, minEn, maxEn)
 		plt.show()
+		for n in range(1,nWfs+1)
+		plotNiuColor(dirpath,"f2response.txt",n)
+		plotNiuColor(dirpath,"f3response.txt",n)
 
 	#look for them
 for dirpat, dirnames, filenames in os.walk("."):
