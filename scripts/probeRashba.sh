89628b6709a88b60eec6d7d55c3b40c55d9ac69c
Maximilian Merte <maximilian.merte@rwth-aachen.de>
2018-03-14 15:56:49 +0100

    small updates

---
+++ scripts/probeRashba.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+root=$PWD
+
+
+
+#set mpi processes
+nProcs=16
+
+
+#data to calculate
+Rashba=( -2.0 0.0 1.0 1.5 2.0 2.5 3.0) #10.0 100.0 1000.0 )
+
+
+
+
+#set numerical paras
+gCut=25.0	#set Gcut
+nSolve=96
+#nQx=16
+nShell=1
+
+#
+nSCx=$((nQx+1))
+nKx=$((nQx+1))
+
+
+#set numerics
+infoString='# changed by probeB.sh'
+sed -i  "/Gcut/c\   Gcut        = $gCut 	$infoString"	 	./input.txt
+sed -i  "/nSolve/c\   nSolve      = $nSolve 	$infoString"	./input.txt
+#sed -i 	"/nQx/c\   nQx         = $nQx 	$infoString" 			./input.txt
+#sed -i 	"/nQy/c\   nQy         = $nQx 	$infoString" 			./input.txt
+#sed -i 	"/nSCx/c\   nSCx        = $nSCx 	$infoString" 		./input.txt
+#sed -i 	"/nSCy/c\   nSCy        = $nSCx 	$infoString" 		./input.txt
+#sed -i 	"/nKx/c\   nKx         = $nKx 	$infoString" 			./input.txt
+#sed -i 	"/nKy/c\   nKy         = $nKx 	$infoString" 			./input.txt
+sed -i 	"/shells/c\   shells       = $nShell 	$infoString" 	./input.txt
+
+#set Hamiltionian
+sed -i 	"/doVdesc/c\	doVdesc		= f 	$infoString"		./input.txt
+sed -i 	"/doZeeman/c\	doZeeman	= f 	$infoString"		./input.txt
+sed -i 	"/doMagHam/c\	doMagHam	= f 	$infoString"		./input.txt
+sed -i "/doRashba/c\    doRashba    = t     $infoString"        ./input.txt
+
+#folder to save data
+label='RashbaTest'
+mkdir -p $label
+now="$(date +"%T")"
+echo '['$now']: created directory '$label
+printf "*\n*\n"
+
+
+
+
+
+
+
+#body
+for aRash in ${Rashba[*]}; do
+	#
+	echo '['$(date +"%T")']: start aRashba='$aRash'eV ang; '
+	#
+	#creates directories
+	dir=$label'/a'$aRash
+	if [[ ! -e $dir ]]; then
+   	 	mkdir $dir
+   	 	echo '['$(date +"%T")']: created dir '$dir
+	else
+    	rm -r -f $dir/*
+    	echo '['$(date +"%T")']: '$dir' was wiped'
+	fi
+	#
+	#cpy relevant files to subdirectory
+	cp main.exe $dir
+	cp input.txt $dir
+	cp subStream.sh $dir
+	wait
+	#
+	#set magnetic field amplitude
+	cd $dir
+	sed -i "/aRashba/c\    aRashba  =  $aRash " ./input.txt
+	wait
+	#
+	#execute calculation
+	./subStream.sh $nProcs
+	wait
+	echo '['$(date +"%T")']: finished subStream'
+	#
+	rm ./main.exe
+	rm ./subStream.sh
+	rm -r w90files
+	rm -r output
+	rm -r oldInput
+	rm -r rawData
+	rm ./input.txt
+	wait
+	cp results/* ./
+	wait
+	rm -r results
+
+
+	#jump back to root
+	cd $root
+	echo '['$(date +"%T")']: done current calc'
+	printf '*\n*\n'
+done
+
+echo '['$(date +"%T")']: all done, by'
