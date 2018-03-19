cf906f1da20e85990b595bad0baf7cfc8952a334
Maximilian Merte <maximilian.merte@rwth-aachen.de>
2018-03-19 11:06:14 +0100

    set qx and qy, switch for rashba

--- scripts/exec/probeB.sh
+++ scripts/exec/probeB.sh
@@ -16,12 +16,15 @@ Bz=( -10.0 -5.0 -1.0 0.0 1.0 2.0 3.0 4.0 5.0 6.0 7.0 8.0 9.0 10.0 25.0 ) #10.0 1
 #set numerical paras
 gCut=28.0	#set Gcut
 nSolve=96
-nQx=16
+nQx=8
+nQy=16
+nKx=33
+nKy=65
 nShell=1
 
 #
 nSCx=$((nQx+1))
-nKx=$((nQx+1))
+nSCy=$((nQy+1))
 
 
 #set numerics
@@ -29,18 +32,18 @@ infoString='# changed by probeB.sh'
 sed -i  "/Gcut/c\   Gcut        = $gCut 	$infoString"	 	./input.txt
 sed -i  "/nSolve/c\   nSolve      = $nSolve 	$infoString"	./input.txt
 sed -i 	"/nQx/c\   nQx         = $nQx 	$infoString" 			./input.txt
-sed -i 	"/nQy/c\   nQy         = $nQx 	$infoString" 			./input.txt
+sed -i 	"/nQy/c\   nQy         = $nQy 	$infoString" 			./input.txt
 sed -i 	"/nSCx/c\   nSCx        = $nSCx 	$infoString" 		./input.txt
-sed -i 	"/nSCy/c\   nSCy        = $nSCx 	$infoString" 		./input.txt
+sed -i 	"/nSCy/c\   nSCy        = $nSCy 	$infoString" 		./input.txt
 sed -i 	"/nKx/c\   nKx         = $nKx 	$infoString" 			./input.txt
-sed -i 	"/nKy/c\   nKy         = $nKx 	$infoString" 			./input.txt
+sed -i 	"/nKy/c\   nKy         = $nKy 	$infoString" 			./input.txt
 sed -i 	"/shells/c\   shells       = $nShell 	$infoString" 	./input.txt
 
 #set Hamiltionian
 sed -i 	"/doVdesc/c\	doVdesc		= f 	$infoString"		./input.txt
 sed -i 	"/doZeeman/c\	doZeeman	= t 	$infoString"		./input.txt
 sed -i 	"/doMagHam/c\	doMagHam	= f 	$infoString"		./input.txt
-
+sed -i "/doRashba/c\	doRashba	= f		$infoString"		./input.txt
 
 #folder to save data
 label='Bprobe'
