300b1b5bbd74453c575accf62dba73c5c6d142b5
Maximilian Merte <maximilian.merte@rwth-aachen.de>
2018-05-17 14:25:36 +0200

    also copy wpout to results folrder

--- scripts/exec/subStream.sh
+++ scripts/exec/subStream.sh
@@ -101,6 +101,7 @@ function runCalc {
 	cp w90files/*geninterp.dat results/
 	cp w90files/*.xsf results/
 	cp w90files/*.wout results/
+	cp w90files/*.wpout results/
 	mv output/f2response.txt results/
 	mv output/f3response.txt results/
 
